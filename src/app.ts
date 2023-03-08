eee5013044824d8cb7db51285bee23b3d689847e
jasonluo <yulaie1012@gmail.com>
2023-03-09 00:15:02 +0800

    chore: add ESLint and Prettier

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,13 @@ dotenv.config();
 
 const app = express();
 const HOST = process.env.HOST || 'localhost';
-const PORT = parseInt(process.env.PORT || '3000');
+const PORT = parseInt(process.env.PORT || '3000', 10);
 
 app.get('/', (_req, res) => {
   res.status(200).json({ status: 'success', message: 'Hello World!' });
 });
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`App listening on port http://${HOST}:${PORT}`);
 });
