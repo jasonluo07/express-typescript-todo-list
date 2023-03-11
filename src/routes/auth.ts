c545f577f1143f79901031b510271519600edcec
jasonluo <yulaie1012@gmail.com>
2023-03-11 18:10:37 +0800

    feat: add auth routes for registering and logging

---
+++ src/routes/auth.ts
@@ -0,0 +1,57 @@
+import { Router } from 'express';
+import bcrypt from 'bcrypt';
+import User, { IUser } from '../models/user';
+import { ApiResponseStatus } from '../types/apiResponse';
+import { respond } from '../utils/apiResponse';
+import { StatusCodes } from 'http-status-codes';
+
+const router = Router();
+
+router.post('/register', async (req, res) => {
+  try {
+    // 檢查是否已存在該電子郵件的使用者
+    const existingUser = await User.findOne({ email: req.body.email });
+    if (existingUser) {
+      return respond(res, StatusCodes.BAD_REQUEST, ApiResponseStatus.Fail, 'Email already registered', null);
+    }
+
+     // 建立新的使用者
+    const hashedPassword = await bcrypt.hash(req.body.password, 12);
+    const user: IUser = new User({
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    await user.save();
+
+    return respond(res, StatusCodes.CREATED, ApiResponseStatus.Success, 'User registered successfully', user);
+  } catch (err) {
+    console.error(err);
+    return respond(res, StatusCodes.INTERNAL_SERVER_ERROR, ApiResponseStatus.Error, 'Failed to registered user', null);
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    // 檢查是否已存在該使用者
+    const user: IUser | null = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return respond(res, StatusCodes.UNAUTHORIZED, ApiResponseStatus.Fail, 'Invalid email', null);
+    }
+
+    // 檢查是否密碼正確
+    const isPasswordValid = await bcrypt.compare(req.body.password, user.password);
+    if (!isPasswordValid) {
+      return respond(res, StatusCodes.UNAUTHORIZED, ApiResponseStatus.Fail, 'Invalid password', null);
+    }
+
+    // 生成 token
+    // ...
+
+    return respond(res, StatusCodes.OK, ApiResponseStatus.Success, 'User logged in successfully', user);
+  } catch (err) {
+    console.error(err);
+    return respond(res, StatusCodes.INTERNAL_SERVER_ERROR, ApiResponseStatus.Error, 'Failed to log in', null);
+  }
+});
+
+export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import auth from './auth';
 import todos from './todos';
 import { ApiResponseStatus } from '../types/apiResponse';
 import { respond } from '../utils/apiResponse';
@@ -10,6 +11,7 @@ router.get('/', (_req, res) => {
   return respond(res, StatusCodes.OK, ApiResponseStatus.Success, 'Hello World!', null);
 });
 
+router.use('/api', auth);
 router.use('/api/todos', todos);
 
 export default router;
