7bc5958ba27b74784296fe305136db0a2c375402
jasonluo <yulaie1012@gmail.com>
2023-03-15 01:23:13 +0800

    chore: update apiResponse to use StatusCodes instead of number codes

--- src/utils/apiResponse.ts
+++ src/utils/apiResponse.ts
@@ -1,10 +1,10 @@
 import { Response } from 'express';
-
+import { StatusCodes } from 'http-status-codes';
 import { ApiResponseStatuses, ApiResponse } from '../types/apiResponse';
 
 export default function respond<T>(
   res: Response,
-  code: number,
+  code: StatusCodes,
   status: ApiResponseStatuses,
   message: string,
   data: T | null,
