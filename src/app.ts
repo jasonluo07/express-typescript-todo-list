ac5b9b47c8bd078026c19e3fa74ca5b8350e7946
jasonluo <yulaie1012@gmail.com>
2023-03-18 20:51:52 +0800

    chore

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import i18n from './middlewares/i18nMiddleware';
+import i18nMiddleware from './middlewares/i18nMiddleware';
 
 import router from './routes';
 
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json()); // 解析 json 格式的 req.body
-app.use(i18n); // 設定多語系
-app.use(router); // 設置路由器
+app.use(i18nMiddleware); // 設定多語系
+app.use(router); // 設定路由器
 
 export default app;
