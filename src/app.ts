17eb52aa25973cf371eed1ca1de4545f242ce931
Samar <s99502042@gmail.com>
2023-04-05 01:02:56 +0800

    feat:add cors options

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import cors from 'cors';
 import i18nMiddleware from './middlewares/i18nMiddleware';
 import errorHandleingMiddleware from './middlewares/errorHandlingMiddleware';
 
@@ -10,6 +11,14 @@ dotenv.config(); // Load .env file
 const app = express(); // Create express app
 
 app.use(express.json()); // Parse JSON body
+
+const allowedOrigins = ['http://localhost:3000']; // list of origins allow to access resources on server
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
+
+app.use(cors(options));
 app.use(i18nMiddleware); // Set i18n middleware
 app.use(router); // Set router
 app.use(errorHandleingMiddleware); // Set error handling middleware
