8cd286594ebb8fee3631563ef44e6f33b49d85be
shiwei <yulaie1012@gmail.com>
2023-04-24 20:56:29 +0800

    perf: add url to the list of allowed origins

--- src/configs/corsOptions.ts
+++ src/configs/corsOptions.ts
@@ -1,6 +1,6 @@
 import { CorsOptions } from 'cors';
 
-const allowedOrigins = ['http://localhost:3000']; // List of origins allowed to access resources on the server
+const allowedOrigins = ['http://localhost:3000', 'https://react-typescript-todo-list.onrender.com']; // List of origins allowed to access resources on the server
 
 const corsOptions: CorsOptions = {
   origin: allowedOrigins,
