01cf3a6f64569b3d1a40994d9fb7350a7935eb0f
jasonluo07 <yulaie1012@gmail.com>
2023-04-25 21:53:40 +0800

    refactor: custimize StatusCode enum not using 3rd package

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,17 @@
-import { StatusCodes as StatusCode } from 'http-status-codes';
 import { Document } from 'mongoose';
 
-export { StatusCode };
+export enum StatusCode {
+  OK = 200,
+  CREATED = 201,
+  NO_CONTENT = 204,
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  FORBIDDEN = 403,
+  NOT_FOUND = 404,
+  CONFLICT = 409,
+  UNPROCESSABLE = 422,
+  INTERNAL_SERVER_ERROR = 500,
+}
 
 export enum ApiStatus {
   SUCCESS = 'Success',
