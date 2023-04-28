8d0ac6d03da30480dd7a89b7e2a6501239ba50e7
jason <yulaie1012@gmail.com>
2023-04-28 21:19:33 +0800

    refactor: add index to controllers directory

---
+++ src/controllers/index.ts
@@ -0,0 +1,5 @@
+import authController from './authController';
+import hellosController from './hellosController';
+import todosController from './todosController';
+
+export { authController, hellosController, todosController };
